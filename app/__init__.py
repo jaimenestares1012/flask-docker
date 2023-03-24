f26d1d8104b3d75eaa2708e5dc76c103e678cd2d
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-03-23 21:51:24 -0500

    ss

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .controllers.fise import test_fise, controlador2_bp
 
 app = Flask(__name__)
 
-
+print("sssssssssssssssssss")
 
 app.register_blueprint(test_fise)
 app.register_blueprint(controlador2_bp)
